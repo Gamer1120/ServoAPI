fec8b77b058156979ad9ad586a69f680cf19a962
Gamer1120 <michaelkoopmantest@gmail.com>
2021-03-23 15:52:02 +0100

    Create turn180.py

---
+++ turn180.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.OUT)
+servo1 = GPIO.PWM(11,50)
+servo1.start(0)
+print ("Moving servo!")
+servo1.ChangeDutyCycle(9)
+time.sleep(1)
+servo1.ChangeDutyCycle(11)
+time.sleep(0.5)
+servo1.stop()
+GPIO.cleanup()
